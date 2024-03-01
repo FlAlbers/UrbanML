aa331abb4eea6b335e11389561ab691a639ab0a0
unknown <fl-albers@web.de>
2024-03-01 15:37:49 +0100

    script to download dwd data + write to csv

---
+++ pythonProject/dwd_download.py
@@ -0,0 +1,17 @@
+#import dwdweather as dwd
+import pandas as pd
+from datetime import datetime
+from dwdweather import DwdWeather
+
+# Create client object.
+dwd = DwdWeather(resolution="hourly")
+
+# Find closest station to position.
+closest = dwd.nearest_station(lon=7.0, lat=51.0)
+
+# The hour you're interested in.
+# The example is 2014-03-22 12:00 (UTC).
+query_hour = datetime(2014, 3, 22, 12)
+result = dwd.query(station_id=1766, timeframe = "revent", parameter="kl")
+result = dwd.query(station_id=closest["station_id"], timestamp=query_hour)
+print(result)
