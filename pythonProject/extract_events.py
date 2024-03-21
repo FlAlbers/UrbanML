aa8e132a6e549d94b4a57078e85977246ef880b5
Flemming Albers <fa655601@fh-muenster.de>
2024-03-21 15:39:00 +0100

    swmm_inp clean up + function and loop added to generate inp files for rain events

--- pythonProject/extract_events.py
+++ pythonProject/extract_events.py
@@ -38,68 +38,8 @@ if __name__ == '__main__':
     P_events = pd.read_csv(events_path)
     P_series = pd.read_csv(P_path)
 
-    # Order data in P_events by hN_mm
-    P_events_sort = P_events.sort_values('hN_mm')
+    P_events_sample = P_events.sample(10, random_state=1)
 
-    # reset index of P_events_sort
-    P_events_sort = P_events_sort.reset_index(drop=True)
-
-    n_events = [5,15,20,25,35]
-    distribution = [0, 0.40, 0.70, 0.90, 0.975, 1]
-
-    P_sample = pd.DataFrame()
-    for i in range(len(n_events)):
-        # Select data from P_events based on index range
-        selected_events = P_events_sort[(P_events.index >= len(P_events) * distribution[i]) & (P_events.index < len(P_events) * distribution[i+1])]
-
-        # Select a sample from selected_events
-        P_sample = pd.concat([P_sample, selected_events.sample(n_events[i], random_state=2).sort_values('hN_mm')])
-
-    print(P_sample)
-
-    P_sample = P_sample.reset_index(drop=True)
-    
-    # Plot index vs hN_mm
-    plt.plot(P_sample.index, P_sample['hN_mm'])
-    plt.xlabel('Index')
-    plt.ylabel('hN_mm')
-    plt.title('Index vs hN_mm')
-    plt.show()
-
-    # Plot index vs hN_mm
-    plt.plot(P_events_sort.index, P_events_sort['hN_mm'])
-    plt.xlabel('Index')
-    plt.ylabel('hN_mm')
-    plt.title('Index vs hN_mm')
-    plt.show()
-
+    # test extract_events with 10 random samples
     extract_events(events_path, P_path, save_folder)
-
-
-#####################
-    n_events = [33,33,34]
-    distribution = [0, 0.05, 0.10, 1]
-
-    P_sample = pd.DataFrame()
-    for i in range(len(n_events)):
-        # Select data from P_events based on index range
-        selected_events = P_events_sort[(P_events["hN_mm"] >= P_events["hN_mm"].max() * distribution[i]) & (P_events["hN_mm"] < P_events["hN_mm"].max() * distribution[i+1])]
-
-        # Select a sample from selected_events
-        P_sample = pd.concat([P_sample, selected_events.sample(n_events[i], random_state=1).sort_values('hN_mm')])
-
-    print(len(P_sample))
-
-    # P_sample = P_sample.reset_index(drop=True)
-
-
-    # Plot index vs hN_mm
-    plt.plot(P_sample.index, P_sample['hN_mm'])
-    plt.xlabel('Index')
-    plt.ylabel('hN_mm')
-    plt.title('Index vs hN_mm')
-    plt.show()
-
-
-
 
--- pythonProject/swmm_inp.py
+++ pythonProject/swmm_inp.py
@@ -11,24 +11,16 @@ import swmm_api
 from swmm_api import SwmmInput
 from swmm_api.input_file import section_labels as sections
 from swmm_api.input_file.section_types import SECTION_TYPES
+from swmm_api.input_file.sections.others import RainGage
 
 from swmm_api.input_file.sections import Timeseries
 from swmm_api.input_file.sections.others import TimeseriesData, TimeseriesFile
 
-
-# get current path of working directory
-current_path = os.getcwd()
-# path to kostra data
-kostra_data_path = os.path.join(current_path, 'pythonProject\\kostra_118111.csv')
-# read kostra data
-kostra = pd.read_csv(kostra_data_path, delimiter=',', index_col=0)
-
-
 # helper function to get euler model rain series from kostra table
-def get_euler_ts(kostra, return_period, duration, interval = 5, euler_typ = 2, start_time = '2024-01-01 00:00'):
+def get_euler_ts(kostra_data, return_period, duration, interval = 5, euler_typ = 2, start_time = '2024-01-01 00:00'):
     # kostra = pd.read_csv(kostra_data_path, delimiter=',', index_col=0)
     model_rain = RainModeller()
-    model_rain.idf_table = kostra
+    model_rain.idf_table = kostra_data
     model_rain.idf_table.columns = model_rain.idf_table.columns.astype(int)
     ts = model_rain.euler.get_time_series(return_period=int(return_period), duration=int(duration), interval=int(interval), kind = int(euler_typ), start_time=start_time)
     ts = ts.round(2)
@@ -36,38 +28,64 @@ def get_euler_ts(kostra, return_period, duration, interval = 5, euler_typ = 2, s
     return ts
 
 # helper function to add euler model rain series to inp file
-def euler_to_inp(input,kostra, return_period, duration, interval = None, euler_typ = None, start_time = '',TSname = 'Kostra'):
-    euler2 = get_euler_ts(kostra, return_period=return_period, duration=duration, interval=interval, euler_typ=euler_typ, start_time=start_time)
-    # name = f'e2_T{int(return_period)}D{int(duration)}'
+def euler_to_inp(SWMM_inp,kostra_data, return_period, duration, interval = 5, euler_typ = None, start_time = '',TSname = 'Kostra'):
+    euler2 = get_euler_ts(kostra_data, return_period=return_period, duration=duration, interval=interval, euler_typ=euler_typ, start_time=start_time)
+    # Convert TSinterval to swmm format 
+    if interval >= 10:
+        TSinterval_time = f'0:{interval}'
+    else:
+        TSinterval_time = f'0:0{interval}'
     euler2.name = TSname
     # input[sections.TIMESERIES].add_obj(TimeseriesData(name, data=list(zip(euler2.index,euler2))))
-    input[sections.TIMESERIES][TSname] =  TimeseriesData(TSname, data=list(zip(euler2.index,euler2)))
-    return input
+    SWMM_inp[sections.TIMESERIES][TSname] =  TimeseriesData(TSname, data=list(zip(euler2.index,euler2)))
+    SWMM_inp['RAINGAGES'][TSname] = RainGage(TSname, 'VOLUME', interval, 1 ,'TIMESERIES', TSname)
+    return SWMM_inp
+
+def event_to_inp(SWMM_inp, event_data, start_time='2024-01-01 00:00', interval=5 , TSname='Event'):
+    event_data['date'] = pd.to_datetime(event_data['date'])
+    event_data['date'] = event_data['date'].apply(lambda x: start_time + pd.Timedelta(minutes=(x - event_data['date'].iloc[0]).total_seconds() / 60))
+    SWMM_inp[sections.TIMESERIES][TSname] = TimeseriesData(TSname, data=list(zip(event_data['date'], event_data['precipitation_height'])))
+    #Convert TSinterval to swmm format 
+    if interval >= 10:
+        TSinterval_time = f'0:{interval}'
+    else:
+        TSinterval_time = f'0:0{interval}'
+    SWMM_inp['RAINGAGES'][TSname] = RainGage(TSname, 'VOLUME', TSinterval_time, 1 ,'TIMESERIES', TSname)
+    return SWMM_inp
+
+# get current path of working directory
+current_path = os.getcwd()
+
 
 ## Input parameters for inp file generation
+# path to kostra data
+kostra_data_path = os.path.join(current_path, 'pythonProject\\kostra_118111.csv')
+# read kostra data
+kostra = pd.read_csv(kostra_data_path, delimiter=',', index_col=0)
 # get return preiods and durations from kostra table
 jaerlichkeiten = kostra.columns.astype(int)
 # duration needs to be larger than 15min
 dauern = kostra.index[kostra.index >= 15]
+# Name of the study area
 name_place = 'Gievenbeck'
+# Path to save the inp files
 save_inp_path = os.path.join(current_path, 'pythonProject\\inp')
+# Euler type for Kostra data
 euler_typ = 2
+# Start time of the simulation
 start_time = pd.to_datetime('2024-01-01 00:00')
+# Buffer time before and after the rainfall event
 buffer_time = pd.Timedelta('2h')
 end_time = start_time + pd.Timedelta(minutes=int(max(dauern))) + buffer_time * 2
-TSname = 'Kostra'
+# Name of the Kostra time series to be included in the inp file
+TSnameKostra = 'Kostra'
+# Name of the measured time series to be included in the inp file
+TSnameEvent = 'FMO'
+# Time interval of the time series in minutes
 TSinterval = 5
-
-
-
-#Convert TSinterval to swmm format 
-if TSinterval >= 10:
-    TSinterval_time = f'0:{TSinterval}'
-else:
-    TSinterval_time = f'0:0{TSinterval}'
-
-# read inp file
+# read base inp file
 inp_base = swmm_api.read_inp_file('pythonProject\\swmm_Gievenbeck.inp')
+
 
 # Update OPTIONS of inp file
 inp_base['OPTIONS'].update({'START_DATE': start_time.date()})
@@ -76,40 +94,47 @@ inp_base['OPTIONS'].update({'REPORT_START_DATE': start_time.date()})
 inp_base['OPTIONS'].update({'REPORT_START_TIME': start_time.time()})
 inp_base['OPTIONS'].update({'END_DATE': end_time.date()})
 inp_base['OPTIONS'].update({'END_TIME': end_time.time()})
-inp_base['RAINGAGES']['Kostra']['form'] = 'VOLUME'
-inp_base['RAINGAGES']['Kostra']['interval'] = TSinterval_time 
-inp_base['RAINGAGES']['Kostra']['source'] = 'TIMESERIES'
-inp_base['RAINGAGES']['Kostra']['timeseries'] = TSname
 
-# type(inp_base[sections.TIMESERIES])
-# inp_base[sections.TIMESERIES]['Kostra']
-# inp_base[sections.TIMESERIES]['e2_T2D120']
-
-# inp_base = euler_to_inp(inp_base,kostra, return_period=2, duration=120, interval=5, euler_typ=euler_typ, start_time=start_time + buffer_time)
-
-
-#get all euler model rain series for all return periods and durations
+# get all euler model rain series for all return periods and durations
 for j in jaerlichkeiten:
     for d in dauern:
         inp = inp_base
-        
-        inp = euler_to_inp(inp,kostra, return_period=j, duration=d, interval=5, euler_typ=euler_typ, start_time=start_time + buffer_time)
+        inp['TITLE'] = f'{name_place}_e{euler_typ}_T{int(j)}D{int(d)}' 
+        inp = euler_to_inp(inp,kostra, return_period=j, duration=d, interval=5, euler_typ=euler_typ, start_time=start_time + buffer_time, TSname=TSnameKostra)
         for subcatchment in inp['SUBCATCHMENTS']:
-            inp['SUBCATCHMENTS'][subcatchment].rain_gage = TSname
-        ## Activate code below to choose relative end time
-        # end_time = start_time + pd.Timedelta(minutes=int(d)) + buffer_time * 2
-        # inp['OPTIONS'].update({'END_DATE': end_time.date()})
-        # inp['OPTIONS'].update({'END_TIME': end_time.time()})
-        
+            inp['SUBCATCHMENTS'][subcatchment].rain_gage = TSnameKostra        
         inp.write_file(os.path.join(save_inp_path,f'{name_place}_e{euler_typ}_T{int(j)}D{int(d)}.inp'))
-        # inp[sections.TIMESERIES].update(inp_base[sections.TIMESERIES])
 
 
+# Create inp-files with selected rainevents
+inp = inp_base
+folder_path = 'pythonProject\\events_FMO'
+for file_name in os.listdir(folder_path):
+    if file_name.endswith('.csv'):
+        
+        file_path = os.path.join(folder_path, file_name)
+        event_data = pd.read_csv(file_path)
+        inp = event_to_inp(inp_base, event_data, start_time=start_time + buffer_time, TSname=TSnameEvent)
+        for subcatchment in inp['SUBCATCHMENTS']:
+            inp['SUBCATCHMENTS'][subcatchment].rain_gage = TSnameEvent
+        file_name = file_name.replace('.csv', '')
+        inp['TITLE'] = f'{name_place}_{file_name}'
+        inp.write_file(os.path.join(save_inp_path,f'{name_place}_{file_name}.inp'))
 
+# inp = inp_base
+# folder_path = 'pythonProject\\events_FMO'      
+# file_path = os.path.join(folder_path, '2014-02-01 07 20 00_hN1.56.csv')
+# event_data = pd.read_csv(file_path)
+# for subcatchment in inp['SUBCATCHMENTS']:
+#     inp['SUBCATCHMENTS'][subcatchment].rain_gage = TSnameEvent
+# inp = event_to_inp(inp_base, event_data, start_time=start_time + buffer_time, TSname=TSnameEvent)
 
+# inp.write_file(os.path.join(save_inp_path,'2014-02-01 07 20 00_hN1.56.inp'))
 
+# event_data = event_data.set_index('date')
+# TimeseriesData(TSnameEvent, data=list(zip(event_data.index, event_data['precipitation_height'])))
+# inp[sections.TIMESERIES][TSnameEvent] = TimeseriesData(TSnameEvent, data=list(zip(event_data.index, event_data['precipitation_height'])))
 
-
-
-
+# inp[sections.TIMESERIES][TSnameEvent]
+# inp_base['TITLE']
 # gievenbeck = swmm_api.read_inp_file('pythonProject\\swmm_Gievenbeck.inp')
